3de91ae4ed2429f46120f8fc896f071b08e7a3c0
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-07-30 23:59:16 +0700

    Create model Post_has_tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190730164955) do
+ActiveRecord::Schema.define(version: 20190730165823) do
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20190730164955) do
     t.index ["comment_id"], name: "index_likes_on_comment_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "post_has_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_has_tags_on_post_id"
   end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -74,6 +81,7 @@ ActiveRecord::Schema.define(version: 20190730164955) do
   add_foreign_key "likes", "comments"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "post_has_tags", "posts"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
 end
