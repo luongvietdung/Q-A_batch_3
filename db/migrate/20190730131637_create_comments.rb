e464796de10baa2e5dffc660db9e84e65ab2297e
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-07-30 20:17:30 +0700

    Create model Comment

---
+++ db/migrate/20190730131637_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.integer :user_id
+      t.integer :post_id
+      t.text :content_comment
+      t.string :image_comment
+
+      t.timestamps
+    end
+  end
+end
