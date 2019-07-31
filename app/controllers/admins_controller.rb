1048d1bd11a3cbf17a5b5d42bb67311d7ad694e2
dangvuanh <dangvuanh98.dev@gmail.com>
2019-07-31 12:59:28 +0700

    create user account admin

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,29 @@
+class AdminsController < ApplicationController
+	layout 'admin'
+	
+	def index		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			@user.update(admin: :admin)
+			redirect_to '/admins'
+		else
+			render 'new'
+		end
+	end
+
+	def destroy	
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password_digest)                               
+	end
+end
