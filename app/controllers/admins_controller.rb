2825bac474a373e570600e5a7df31f458e91586f
dangvuanh <dangvuanh98.dev@gmail.com>
2019-07-31 08:45:08 +0700

    create account user

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,18 +1,32 @@
 class AdminsController < ApplicationController
+	layout 'admin'
 	def index
 		
 	end
 
 	def new
-		
+		@user = User.new
 	end
 
 	def create
-		
+		@user = User.new(user_params)
+		if @user.save
+			@user.update(admin: 1)
+			redirect_to '/admins'
+		else
+			render 'new'
+		end
 	end
 
 	def destroy
 
 		
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password_digest)
+                                   
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 resources :admins
+post '/admins/signup',  to: 'admins#create'
 end
