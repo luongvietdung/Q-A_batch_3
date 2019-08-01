9c01d8fc3a20654b64c5e814ee326e4d86a411b0
Ngoc Hoang <hoangcnpd01980@fpt.edu.vn>
2019-08-01 11:31:08 +0700

    Design form login register

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,4 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   enum admin: {member: 0, admin: 1}
-
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
-
-  before_save { email.downcase! }
-  has_secure_password
-
-  #Returns the has digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
-  end
 end
